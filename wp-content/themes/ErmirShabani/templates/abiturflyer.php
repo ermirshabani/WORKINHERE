f781c3665f97b2626a32dedd024ff7265446dfec
ermirshabani <e.shabani2015@gmail.com>
2020-09-18 19:07:56 +0200

    223444

--- wp-content/themes/ErmirShabani/templates/abiturflyer.php
+++ wp-content/themes/ErmirShabani/templates/abiturflyer.php
@@ -39,7 +39,7 @@ Template Name: Abiturflyer
                     Die Vorbereitung und der Unterricht
                     erfolgt auf Grundlage der GSO sowie des
                     bayerischen Lehrplans für Unterricht und
-                    Kultus.</p><p>GET COMMIT</p>
+                    Kultus.</p><p>GET COMMIT11111222</p>
             </div>
             <!-- MBARIMI I KOLONES SE TRETE -->
 
